623a9825d8bf236a652a3851d1c1c8e04ebc026c
Arystan Muzdakhanov <amuzdakhanov@rambler.ru>
2022-04-04 15:06:35 +0600

    added 7.1

---
+++ 7.1_cicl_for/program3.py
@@ -0,0 +1,34 @@
+"""
+Напишите программу,
+которая использует ровно три цикла for для печати следующей последовательности символов
+AAA
+AAA
+AAA
+AAA
+AAA
+AAA
+BBBB
+BBBB
+BBBB
+BBBB
+BBBB
+E
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+G
+"""
+for i in range (6):
+    print('AAA')
+for i in range (5):
+    print('BBBB')
+print('E')
+for i in range (9):
+    print('TTTTT')
+print('G')
