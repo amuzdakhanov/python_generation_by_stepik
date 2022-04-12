a794e316d9a73e4a2f9a849b3ca2b7ccac8f425f
Arystan Muzdakhanov <amuzdakhanov@rambler.ru>
2022-04-12 17:26:11 +0600

    7.7 modified

--- 7.7_review_koda/program4.py
+++ 7.7_review_koda/program4.py
@@ -7,14 +7,14 @@
 Известно, что каждая ошибка затрагивает только одну строку и может быть исправлена без изменения других строк.
 """
 n = int(input())
-max_digit = n % 10
+max_digit = -1
 while n > 0:
     digit = n % 10
     if digit % 3 == 0:
-        if digit < max_digit:
-            digit = max_digit
-    n = n % 10
-if max_digit == 0:
+        if digit > max_digit:
+            max_digit = digit
+    n = n // 10
+if max_digit == -1:
     print('NO')
 else:
     print(max_digit)
