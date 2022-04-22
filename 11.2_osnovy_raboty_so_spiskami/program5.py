f8b4335dfb7c4e3ed507f70200e2b3f42667b50a
Arystan Muzdakhanov <amuzdakhanov@rambler.ru>
2022-04-22 17:20:00 +0600

    added 1.1-1.3

---
+++ 11.2_osnovy_raboty_so_spiskami/program5.py
@@ -0,0 +1,8 @@
+"""
+Дополните приведенный код так, чтобы он вывел среднее арифметическое элементов списка evens.
+"""
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+sum_n = sum(evens)
+count = len(evens)
+average = sum_n/count
+print(average)
---
+++ 11.2_osnovy_raboty_so_spiskami/program7.py
@@ -0,0 +1,6 @@
+"""
+Дополните приведенный код так, чтобы элемент списка имеющий значение Green заменился на значение Зеленый,
+а элемент Violet на Фиолетовый. Далее необходимо вывести полученный список.
+"""
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
